a3f1b0367ef88ef7de9866a49d50bff3016eeb7b
arnab <arnhazra@gmail.com>
2025-02-14 17:12:53 +0530

    publish

--- apps/uiservice/src/app/(platform)/model/[id]/page.tsx
+++ apps/uiservice/src/app/(platform)/model/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { BookMarked, Copy, Medal } from "lucide-react"
+import { BookMarked, Copy, MessageCircleIcon } from "lucide-react"
 import { Badge } from "@/shared/components/ui/badge"
 import { Button } from "@/shared/components/ui/button"
 import {
@@ -23,6 +23,7 @@ import { use } from "react"
 import Loading from "@/app/loading"
 import { UseQueryResult } from "@tanstack/react-query"
 import { DerivedModel } from "@/shared/types"
+import { ModelCard } from "@/shared/components/modelcard"
 
 export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id: modelId = "" } = use(params)
@@ -33,13 +34,29 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
     method: HTTPMethods.GET,
   })
 
+  const relatedModels: UseQueryResult<DerivedModel[], Error> = useFetch({
+    queryKey: ["related-models", model?.data?.category ?? ""],
+    queryUrl: endPoints.getDerivedModels,
+    method: HTTPMethods.POST,
+    requestBody: {
+      searchQuery: "",
+      selectedFilter: model?.data?.category ?? "",
+      selectedSortOption: "displayName",
+      offset: 0,
+    },
+  })
+
   const renderModelTags = model?.data?.description
     ?.split(" ")
     .slice(0, 30)
     .map((item: string, index: number) => {
       if (item.length > 5) {
         return (
-          <Badge className="me-2 mb-2" variant="outline" key={index}>
+          <Badge
+            className="me-2 mb-2 bg-zinc-800 hover:bg-zinc-800 ps-2 pe-2 pt-1 pb-1"
+            variant="default"
+            key={index}
+          >
             {item}
           </Badge>
         )
@@ -56,47 +73,62 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
     })
   }
 
-  const dataQuality = (rating: number): string => {
-    if (rating > 4.5) return "Gold"
-    if (rating > 4.0) return "Silver"
-    return "Bronze"
-  }
+  const renderRelatedModels = relatedModels?.data?.map((model) => {
+    return <ModelCard key={model._id} model={model} />
+  })
 
   return (
     <Show condition={!model.isLoading} fallback={<Loading />}>
       <div className="grid flex-1 items-start gap-4 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
         <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
           <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-            <Card className="sm:col-span-2 pb-4 bg-zinc-900 text-white border-zinc-800">
+            <Card className="sm:col-span-4 pb-4 text-white bg-zinc-900 border-zinc-800">
               <CardHeader className="pb-3">
                 <CardTitle className="flex justify-between">
                   {model?.data?.displayName}
-                  <ActivityLog keyword={modelId} />
+                  <div className="flex gap-4">
+                    <ActivityLog keyword={modelId} />
+                    <Show condition={!!model?.data?.isPro}>
+                      <Badge className="bg-lime-500">Pro</Badge>
+                    </Show>
+                  </div>
                 </CardTitle>
-                <CardDescription className="max-w-lg justify-normal text-white">
-                  {model?.data?.description.slice(0, 200)}...
+                <CardDescription className="justify-normal text-zinc-200">
+                  {model?.data?.description}
                 </CardDescription>
               </CardHeader>
+              <CardContent className="flex gap-4 mt-4 -mb-4">
+                <Button className="bg-zinc-800 hover:bg-zinc-800">
+                  <MessageCircleIcon className="me-2 scale-75" />
+                  Open Playground
+                </Button>
+                <Button className="bg-zinc-800 hover:bg-zinc-800">
+                  <MessageCircleIcon className="me-2 scale-75" />
+                  Chat with this model
+                </Button>
+              </CardContent>
             </Card>
           </div>
-          <p className="text-xl ms-1 -mb-2 -mt-2">Related Models</p>
-          {/* <div className="mx-auto grid justify-center gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+          <p className="text-xl ms-1 -mb-4 -mt-2 text-white">Related Models</p>
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(16rem,1fr))] gap-4 py-4">
             {renderRelatedModels}
-          </div> */}
+          </div>
         </div>
-        <Card className="overflow-hidden">
+        <Card className="overflow-hidden bg-zinc-900 text-white border-zinc-800">
           <CardHeader className="flex flex-row items-start bg-muted/50">
             <div className="grid gap-0.5">
               <CardTitle className="group flex items-center gap-2 text-lg">
                 {model?.data?.displayName}
               </CardTitle>
-              <CardDescription>{model?.data?.category}</CardDescription>
+              <CardDescription className="text-white">
+                {model?.data?.category}
+              </CardDescription>
             </div>
             <div className="ml-auto flex items-center gap-1">
               <Button
                 size="sm"
-                variant="outline"
-                className="h-8 gap-1"
+                variant="default"
+                className="h-8 gap-1 bg-zinc-800 hover:bg-zinc-800"
                 onClick={copyModelId}
               >
                 <Copy className="h-3.5 w-3.5" />
@@ -111,44 +143,54 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
               <div className="font-semibold text-lg">Model Details</div>
               <ul className="grid gap-3">
                 <li className="flex items-center justify-between">
-                  <span className="text-muted-foreground">Model Rating</span>
+                  <span className="text-muted-foreground">Architecture</span>
                   <span>{model?.data?.baseModel.architecture}</span>
                 </li>
                 <li className="flex items-center justify-between">
-                  <span className="text-muted-foreground">Data Volume</span>
+                  <span className="text-muted-foreground">Vendor</span>
+                  <span>{model?.data?.baseModel.vendor}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Base Model</span>
+                  <span>{model?.data?.baseModel.displayName}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Series</span>
+                  <span>{model?.data?.baseModel.series}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Context Window</span>
+                  <span>{model?.data?.baseModel.contextWindow}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">
+                    Default Temerature
+                  </span>
+                  <span>{model?.data?.baseModel.defaultTemperature}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Default Top P</span>
+                  <span>{model?.data?.baseModel.defaultTopP}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Parameters</span>
+                  <span>{model?.data?.baseModel.parameters}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Generic Name</span>
                   <span>{model?.data?.baseModel.genericName}</span>
                 </li>
-                {/* <li className="flex items-center justify-between">
-                  <span className="text-muted-foreground">Data Quality</span>
-                  <span>
-                    <Show condition={model?.data?.metaData?.rating >= 4.5}>
-                      <Badge variant="default" key={"gold"}>
-                        <Medal className="scale-50" />
-                        Gold
-                      </Badge>
-                    </Show>
-                    <Show
-                      condition={
-                        model?.data?.metaData?.rating >= 4.0 &&
-                        model?.data?.metaData?.rating < 4.5
-                      }
-                    >
-                      <Badge variant="secondary" key={"silver"}>
-                        <Medal className="scale-50" />
-                        Silver
-                      </Badge>
-                    </Show>
-                    <Show condition={model?.data?.metaData?.rating < 4.0}>
-                      <Badge variant="outline" key={"bronze"}>
-                        <Medal className="scale-50" />
-                        Bronze
-                      </Badge>
-                    </Show>
-                  </span>
-                </li> */}
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Fine Tuned</span>
+                  <span>{model?.data?.isFineTuned ? "Yes" : "No"}</span>
+                </li>
+                <li className="flex items-center justify-between">
+                  <span className="text-muted-foreground">Response Format</span>
+                  <span>{model?.data?.responseFormat}</span>
+                </li>
               </ul>
             </div>
-            <Separator className="my-4" />
+            <Separator className="my-4 bg-zinc-800" />
             <div className="grid gap-3">
               <div className="font-semibold text-lg">Model Tags</div>
               <div>{renderModelTags}</div>
@@ -157,7 +199,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
           <CardFooter className="flex flex-row items-center bg-muted/50 px-6 py-3">
             <Button
               variant="default"
-              className="w-full"
+              className="w-full bg-zinc-800 hover:bg-zinc-800"
               onClick={(): void => router.push("/apireference/datamarketplace")}
             >
               Data API Reference
