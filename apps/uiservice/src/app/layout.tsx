38d0809ab18c05332287b16aecb086f243421888
arnab <arnhazra@gmail.com>
2025-03-01 09:54:17 +0530

    publish

--- apps/uiservice/src/app/layout.tsx
+++ apps/uiservice/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <title>{brandName}</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta name="theme-color" content="#ffffff" />
         <meta
