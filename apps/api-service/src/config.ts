57b5f3c90224d80c904aaa53de2be5debc6ed155
arnab <arnhazra@gmail.com>
2025-03-23 09:16:50 +0530

    publish

--- apps/api-service/src/config.ts
+++ apps/api-service/src/config.ts
@@ -2,7 +2,6 @@ import { config as envConfig } from "dotenv"
 envConfig({ path: "./.env.development" })
 
 export const config = {
-  API_PORT: process.env.API_PORT,
   NODE_ENV: process.env.NODE_ENV,
   BRAND_NAME: process.env.BRAND_NAME,
   CORE_DATABASE_URI: process.env.CORE_DATABASE_URI,
@@ -12,11 +11,6 @@ export const config = {
   GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
   OTP_HASHING_KEY: process.env.OTP_HASHING_KEY,
-  GCLOUD_REDIRECT_URI: process.env.GCLOUD_REDIRECT_URI,
-  GCLOUD_CLIENT_ID: process.env.GCLOUD_CLIENT_ID,
-  GCLOUD_CLIENT_SECRET: process.env.GCLOUD_CLIENT_SECRET,
-  GCLOUD_REFRESH_TOKEN: process.env.GCLOUD_REFRESH_TOKEN,
-  MAILER_EMAIL: process.env.MAILER_EMAIL,
   JWT_SECRET: process.env.JWT_SECRET,
   NEWS_API_ORG_KEY: process.env.NEWS_API_ORG_KEY,
   AZURE_OPENAI_URI: process.env.AZURE_OPENAI_URI,
--- apps/api-service/src/main.ts
+++ apps/api-service/src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
-import { config } from "./config"
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({ exposedHeaders: ["token"] })
-  await app.listen(config.API_PORT)
+  await app.listen(8000)
 }
 
 bootstrap()
