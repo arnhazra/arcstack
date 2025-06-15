f3ad5b68e0764b9030cffe06261c479ade18ff9d
arnab <arnhazra@gmail.com>
2025-06-15 16:21:28 +0530

    publish

--- apps/api-service/src/intelligence/chat/chat.strategy.ts
+++ apps/api-service/src/intelligence/chat/chat.strategy.ts
@@ -1,13 +1,9 @@
 import { Injectable } from "@nestjs/common"
 import { Thread } from "./schemas/thread.schema"
-import { HttpService } from "@nestjs/axios"
-import { lastValueFrom } from "rxjs"
 import { config } from "@/config"
-import { statusMessages } from "@/shared/constants/status-messages"
 import { ChatGroq } from "@langchain/groq"
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
 import { ChatOpenAI } from "@langchain/openai"
-import { DynamicTool } from "@langchain/core/tools"
 
 interface ChatStrategyType {
   genericName: string
@@ -20,49 +16,6 @@ interface ChatStrategyType {
 
 @Injectable()
 export class ChatStrategy {
-  constructor(private readonly httpService: HttpService) {}
-
-  getCurrentDateTool = new DynamicTool({
-    name: "getCurrentDate",
-    description: "Use this function when user asks for current date",
-    func: async () => {
-      return new Date().toISOString()
-    },
-  })
-
-  searchWebTool = new DynamicTool({
-    name: "searchWeb",
-    description: "look things up online",
-    func: async (searchString: string) => {
-      try {
-        const uri = `${config.GOOGLE_CSE_API_URI}&q=${searchString}`
-        const response = await lastValueFrom(this.httpService.get<any>(uri))
-        const cleanedData = response?.data?.items?.map(
-          (item: any) => item?.title + item?.snippet
-        )
-        return JSON.stringify(cleanedData)
-      } catch (error) {
-        return statusMessages.connectionError
-      }
-    },
-  })
-
-  getWeatherTool = new DynamicTool({
-    name: "getWeather",
-    description: "Use this tool to get current weather for a specific city",
-    func: async (city: string) => {
-      try {
-        const response = await lastValueFrom(
-          this.httpService.get<any>(config.WEATHER_API_URI)
-        )
-
-        return `Current weather in ${response.data.name}: ${JSON.stringify(response.data)}.`
-      } catch (err) {
-        return statusMessages.connectionError
-      }
-    },
-  })
-
   async openaiStrategy({
     genericName,
     temperature,
@@ -81,12 +34,6 @@ export class ChatStrategy {
       },
     })
 
-    const agent = llm.bindTools([
-      this.searchWebTool,
-      this.getCurrentDateTool,
-      this.getWeatherTool,
-    ])
-
     const chatHistory = thread.flatMap((chat) => [
       {
         role: "user",
@@ -99,7 +46,7 @@ export class ChatStrategy {
     ])
 
     if (systemPrompt) {
-      const response = await agent.invoke([
+      const response = await llm.invoke([
         { role: "system", content: systemPrompt },
         ...chatHistory,
         { role: "user", content: prompt },
