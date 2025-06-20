07a2fd049ec7d320fccc51a5e4196763072858e3
arnab <arnhazra@gmail.com>
2025-06-20 10:15:44 +0530

    publish

--- apps/api-service/src/intelligence/basemodel/basemodel.controller.ts
+++ apps/api-service/src/intelligence/basemodel/basemodel.controller.ts
@@ -6,16 +6,23 @@ import {
   Param,
   UseGuards,
   BadRequestException,
+  Request,
 } from "@nestjs/common"
 import { BaseModelService } from "./basemodel.service"
 import { TokenGuard } from "@/shared/auth/token.guard"
 import { CreateBaseModelDto } from "./dto/create-base-model.request.dto"
 import { FindBaseModelsDto } from "./dto/find-dervied-models.request.dto"
 import { sortOptions } from "./data/model-sort-options"
+import { EventEmitter2 } from "@nestjs/event-emitter"
+import { EventsUnion } from "@/shared/utils/events.union"
+import { ModRequest } from "@/shared/auth/types/mod-request.interface"
 
 @Controller("basemodel")
 export class BaseModelController {
-  constructor(private readonly service: BaseModelService) {}
+  constructor(
+    private readonly service: BaseModelService,
+    private readonly eventEmitter: EventEmitter2
+  ) {}
 
   @UseGuards(TokenGuard)
   @Get("filters-and-sort-options")
@@ -50,8 +57,19 @@ export class BaseModelController {
 
   @UseGuards(TokenGuard)
   @Get(":modelId")
-  async findOneBaseModel(@Param("modelId") modelId: string) {
+  async findOneBaseModel(
+    @Request() request: ModRequest,
+    @Param("modelId") modelId: string
+  ) {
+    const {
+      user: { userId },
+    } = request
+
     try {
+      this.eventEmitter.emitAsync(EventsUnion.CreateHistory, {
+        userId,
+        modelId,
+      })
       return await this.service.findOneBaseModel(modelId)
     } catch (error) {
       throw new BadRequestException()
